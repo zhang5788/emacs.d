a24d7d38b3f4deafbfd567b3eaff2f2df6550461
zhang5788 <zhang7354@gmail.com>
2021-05-24 03:34:23 +0800

    add awesome-tab.el

---
+++ lisp/init-tabs.el
@@ -0,0 +1,7 @@
+(use-package awesome-tab
+  :load-path "~/.emacs.d/elpa/awesome-tab"
+  :config
+  (awesome-tab-mode t)
+  :bind (("C-c h" . awesome-tab-backward-tab)
+	 ("C-c j" . awesome-tab-forward-group)
+	 ("C-c l" . awesome-tab-forward-tab)))
